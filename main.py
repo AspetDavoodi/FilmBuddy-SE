90e34e819a27f1e13f40b62033d030dc61753bcd
AspetDavoodi <aspet_davoodi@edu.aua.am>
2019-12-04 01:45:48 +0400

    linked list stored in hashtable

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import json as js
 
-# Brand, Name, ISO, type, usage, is_highend, is_color, is_inProduction
+# Brand, Model, ISO, type, usage, is_highend, is_color, is_inProduction
+
+class LL:
+
+    def __init__(self):
+        self.head = None
+
+    def append(self,roll):
+        newRoll = roll
+        newRoll.set_next(self.head)
+        self.head = newRoll
+        #LIFO linked list append
+
 
 class RollManager:
 
     def __init__(self):
         self.RollData = {}
         self.MaxNumberofBrands = 100
-        self.Hashtable = [None] * self.MaxNumberofBrands
-        self.head = None
-
+        self.Hashtable = [LL()] * self.MaxNumberofBrands
 
     def HashFunction(self, key):
         hashedkey = 0
         for i in key:
             hashedkey = hashedkey + ord(i)
+        hashedkey = hashedkey % self.MaxNumberofBrands
         return hashedkey
-
 
     def InsertBrand(self,key,value):
         hash_key = self.HashFunction(key)
@@ -30,25 +40,35 @@ class RollManager:
 
             for rollData in self.RollData[key]:
                 roll = Roll(key, rollData["model"], rollData["ISO"], rollData["type"], rollData["Usage"], rollData["is_HighEnd"], rollData["is_Color"], rollData["is_inProduction"])
-                if self.head is None:
-                    self.head = roll
-                else:
-                    last = self.head
-                    while last is not None:
-                        last = roll.next
-                    last.next = roll
 
                 self.addRolltoBrand(key,roll)
 
     def addRolltoBrand(self,brand,roll):
         brand = self.HashFunction(brand)
-        self.Hashtable[brand] = roll
-
+        self.Hashtable[brand].append(roll)
 
     def deleteRoll(self,brand,model):
+        brand = self.HashFunction(brand)
+        TMP = self.Hashtable[brand]
+        current = TMP.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.Model == model:
+                found = True
+            else:
+                previous = current
+                current = current.next
+        if current is None:
+            print ("Error")
+        if previous is None:
+            current.head = current.next
+        else:
+            previous.next = current.next #actual deletion happens here
+
+
+    def findRoll(self):
         pass
-
-
 
     def saveJSON(self):
         pass
@@ -69,29 +89,33 @@ class Roll:
         self.next = None #Linked List implementation made more sense than tree or BST as most rolls have values that coincide
 
 
+    def set_next(self,Node):
+        self.next = Node
 
-    def getBrand(self):
-        return self.Brand
+    def get_next(self):
+        return self.next
 
-    def toJSON(self):
-        json = {
-            "model": self.Model,
-            "ISO": self.ISO,
-            "type": self.type,
-            "Usage": self.usage,
-            "is_HighEnd": self.is_highend,
-            "is_Color": self.is_color,
-            "is_inProduction": self.is_inProduction
-        }
-
-        return json
+    # def getBrand(self):
+    #     return self.Brand
+    #
+    # def toJSON(self):
+    #     json = {
+    #         "model": self.Model,
+    #         "ISO": self.ISO,
+    #         "type": self.type,
+    #         "Usage": self.usage,
+    #         "is_HighEnd": self.is_highend,
+    #         "is_Color": self.is_color,
+    #         "is_inProduction": self.is_inProduction
+    #     }
+    #
+    #     return json
 
 
 def main():
     rolls = RollManager()
     rolls.loadRollDataFromJSON()
-
-    print(rolls)
+    rolls.deleteRoll("Kodak","")
 
 main()
 
